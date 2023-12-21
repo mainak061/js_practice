cea67c39b1fe9c77da22d0a2586d727ef992bcea
mainak061 <mainak061@gmail.com>
2023-12-22 01:14:46 +0530

    first day off

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3 + 3);
+
+let name="Mainak"
+let age=20
+let isLoggedIn= true
+let state
+
+// number => 2 to the power 53
+// bigiint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined => not initialized variable, it is like a placeholder for data that will be assigned later
+// symbol => unique
+
+// object
+
+console.log(typeof "mainak");
---
+++ variables.js
@@ -0,0 +1,4 @@
+const accountId = 144553
+let accountEmail = "mainaks433@gmail.com"
+var accountPassword = "12345"
+accountCity= "Alipurduar"
