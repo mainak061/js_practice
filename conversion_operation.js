61c4dd17fe8dfd4264acd3f77ff9d6fe6506f80a
mainak061 <mainak061@gmail.com>
2023-12-24 00:59:18 +0530

    day2

---
+++ conversion_operation.js
@@ -0,0 +1,18 @@
+let score= false
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33"=>33
+// "33abc"=>NaN
+// true=>1 && false=>0
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// ""=>false
+// "Mainak"=> true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
