021b4120a4a133bd817f1b9bf687371aa412576d
mainak061 <mainak061@gmail.com>
2023-12-24 22:05:20 +0530

    2nd day

--- conversion_operation.js
+++ conversion_operation.js
@@ -16,3 +16,26 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//                  operation
+
+let value=3;
+let negValue= -value
+console.log(negValue);
+let str1="Hello"
+let str2=" Mainak"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true);
+console.log(-true);
+// console.log(!true);
+console.log(+"");
+
+let gameCount=100
+gameCount++;
+console.log(gameCount);
